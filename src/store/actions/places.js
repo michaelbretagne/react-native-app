03be948cbfcfa7dbefb5a0130a452cff40d2a300
Michael <michael.bretagne@gmail.com>
2019-03-02 17:33:09 -0800

    Add redux thunk and dispatch async action to add user's data to the backend

--- src/store/actions/places.js
+++ src/store/actions/places.js
@@ -1,11 +1,35 @@
 import { ADD_PLACE, DELETE_PLACE } from "./actionTypes";
 
 export const addPlace = (placeName, location, image) => {
-  return {
-    type: ADD_PLACE,
-    placeName,
-    location,
-    image,
+  return dispatch => {
+    fetch(
+      "https://us-central1-share-awesome-pl-1551158588067.cloudfunctions.net/storeImage",
+      {
+        method: "POST",
+        body: JSON.stringify({ image: image.base64 }),
+      },
+    )
+      .catch(err => console.log(err))
+      .then(res => res.json())
+      .then(parsedRes => {
+        const placeData = {
+          name: placeName,
+          location,
+          image: parsedRes.imageUrl,
+        };
+        return fetch(
+          "https://share-awesome-pl-1551158588067.firebaseio.com/.places.json",
+          {
+            method: "POST",
+            body: JSON.stringify(placeData),
+          },
+        )
+          .catch(err => console.log(err))
+          .then(res => res.json())
+          .then(parsedRes => {
+            console.log(parsedRes);
+          });
+      });
   };
 };
 
